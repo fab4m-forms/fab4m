4e8766941f59779203c0a026edefc2f3d9e412a1
Fabian Sorqvist <fabian@sorqvist.nu>
2022-12-11 08:24:36 +0100

    fix

--- fab4m.org/src/pages/index.js
+++ fab4m.org/src/pages/index.js
@@ -32,7 +32,7 @@ export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`${siteConfig.title}`}
       description="Take the hard parts out of form building!"
     >
       <HomepageHeader />
