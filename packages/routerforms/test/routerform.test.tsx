dfc984eab742233a898133fb1bef7e7b5881610d
Fabian <fabian@sorqvist.nu>
2025-07-06 13:33:51 +0200

    fix tests

--- packages/routerforms/test/routerform.test.tsx
+++ packages/routerforms/test/routerform.test.tsx
@@ -1,10 +1,9 @@
 import "@testing-library/jest-dom";
 import "cross-fetch/polyfill";
-import { createForm, textField, pageBreak, content } from "@fab4m/fab4m";
-import { act, fireEvent, render, waitFor } from "@testing-library/react";
+import { createForm, textField, pageBreak, content, Form } from "@fab4m/fab4m";
+import { fireEvent, render, waitFor } from "@testing-library/react";
 import {
   FormRouteProps,
-  FormRoute,
   StatefulFormRoute,
   RouterFormView,
 } from "../src";
@@ -12,135 +11,143 @@ import { vi } from "vitest";
 import * as React from "react";
 import {
   ActionFunction,
-  createRoutesStub,
-  MemoryRouter,
-  Route,
-  Routes,
-} from "react-router";
+  createMemoryRouter,
+  RouterProvider,
+} from "react-router-dom";
 
 describe("Routed form", () => {
-  const MemoryRouterFormView = (props: FormRouteProps & { path: string }) => (
-    <MemoryRouter initialEntries={[props.path]}>
-      <Routes>
-        <Route
-          path={`/:part`}
-          element={<FormRoute basename="" {...props} />}
-        ></Route>
-        <Route
-          path={`/`}
-          element={<FormRoute basename="" {...props} />}
-        ></Route>
-      </Routes>
-    </MemoryRouter>
-  );
-  const form = createForm({});
-  form.add(textField({ name: "first", label: "Before page break" }));
-  form.add(pageBreak({ label: "First break", name: "first_break" }));
-  form.add(
-    textField({
-      name: "second",
-      label: "After page break",
-    }),
-  );
-  let data = {};
-  form.onComponentChange((property, value) => {
-    data = { ...data, [property]: value };
-  });
-  it("Router form", async () => {
-    const screen = render(
-      <MemoryRouterFormView path="/" form={form} data={data} />,
+  const createTestForm = () => {
+    const form = createForm({});
+    form.add(textField({ name: "first", label: "Before page break" }));
+    form.add(pageBreak({ label: "First break", name: "first_break" }));
+    form.add(
+      textField({
+        name: "second",
+        label: "After page break",
+      }),
     );
-    expect(screen.queryByText("Before page break")).toBeVisible();
+    return form;
+  };
+
+  const MemoryRouterFormView = (
+    props: Omit<FormRouteProps, "form"> & {
+      path: string;
+      form: Form;
+    },
+  ) => {
+    const { path, ...rest } = props;
+    const router = createMemoryRouter(
+      [
+        {
+          path: `/:part`,
+          element: <StatefulFormRoute basename="" {...rest} />,
+        },
+        {
+          path: `/`,
+          element: <StatefulFormRoute basename="" {...rest} />,
+        },
+      ],
+      {
+        initialEntries: [path],
+      },
+    );
+    return <RouterProvider router={router} />;
+  };
+
+  it("Router form", async () => {
+    const form = createTestForm();
+    const screen = render(<MemoryRouterFormView path="/" form={form} />);
+    expect(await screen.findByLabelText("Before page break")).toBeVisible();
     expect(screen.queryByLabelText("After page break")).toBeNull();
     expect(screen.queryByText("Previous")).toBeNull();
     const next = await screen.findByText("Next");
     fireEvent.click(next);
     await waitFor(async () => {
-      expect(screen.queryByLabelText("After page break")).toBeVisible();
+      expect(await screen.findByLabelText("After page break")).toBeVisible();
     });
     const prev = await screen.findByText("Previous");
     fireEvent.click(prev);
     await waitFor(async () => {
-      expect(screen.queryByLabelText("Before page break")).toBeVisible();
+      expect(await screen.findByLabelText("Before page break")).toBeVisible();
     });
   });
-  it("Invalid page should redirect to first page", () => {
-    const { queryByText, queryByLabelText } = render(
-      <MemoryRouterFormView form={form} path="/2" data={data} />,
+
+  it("Invalid page should redirect to first page", async () => {
+    const form = createTestForm();
+    const { findByLabelText, queryByText } = render(
+      <MemoryRouterFormView form={form} path="/2" />,
     );
     expect(queryByText("Previous")).toBeNull();
-    expect(queryByLabelText("Before page break")).toBeVisible();
+    expect(await findByLabelText("Before page break")).toBeVisible();
   });
-  it("Don't allow viewing a page when we haven't completed the previos page", () => {
-    const { queryByText, queryByLabelText } = render(
-      <MemoryRouterFormView form={form} path="/1" data={data} />,
+
+  it("Don't allow viewing a page when we haven't completed the previos page", async () => {
+    const form = createTestForm();
+    const { findByLabelText, queryByText } = render(
+      <MemoryRouterFormView form={form} path="/1" />,
     );
     expect(queryByText("Previous")).toBeNull();
-    expect(queryByLabelText("Before page break")).toBeVisible();
+    expect(await findByLabelText("Before page break")).toBeVisible();
   });
-  it("Allow viewing page 2 when explicitly allowed", () => {
-    const { queryByText, queryByLabelText } = render(
-      <MemoryRouterFormView
-        completedParts={1}
-        form={form}
-        path="/1"
-        data={data}
-      />,
+
+  it("Allow viewing page 2 when explicitly allowed", async () => {
+    const form = createTestForm();
+    const { findByLabelText, findByText } = render(
+      <MemoryRouterFormView completedParts={1} form={form} path="/1" />,
     );
-    expect(queryByText("Previous")).toBeVisible();
-    expect(queryByLabelText("After page break")).toBeVisible();
+    expect(await findByText("Previous")).toBeVisible();
+    expect(await findByLabelText("After page break")).toBeVisible();
   });
+
   it("Form route action", async () => {
+    const form = createTestForm();
     let submitRequest: Request | null = null;
     const action: ActionFunction = async ({ request }) => {
       submitRequest = request;
+      return null;
     };
     const spy = vi.fn().mockImplementation(action);
-    function Route() {
-      const Stub = createRoutesStub([
+    const router = createMemoryRouter(
+      [
         {
           path: "/:part",
           action: spy,
-          Component: () => (
-            <StatefulFormRoute useRouteAction={true} form={form} />
-          ),
+          element: <StatefulFormRoute useRouteAction={true} form={form} />,
         },
         {
           path: "*",
-          Component: () => (
-            <StatefulFormRoute useRouteAction={true} form={form} />
-          ),
+          action: spy,
+          element: <StatefulFormRoute useRouteAction={true} form={form} />,
         },
-      ]);
-      return <Stub initialEntries={["/0"]} />;
-    }
+      ],
+      { initialEntries: ["/0"] },
+    );
 
-    const { findByText, findByLabelText } = render(<Route />);
+    const { findByText, findByLabelText } = render(
+      <RouterProvider router={router} />,
+    );
     fireEvent.input(await findByLabelText("Before page break"), {
-      value: "A text",
       target: { value: "A text" },
     });
     fireEvent.click(await findByText("Next"));
     fireEvent.input(await findByLabelText("After page break"), {
-      value: "Another text",
       target: { value: "Another text" },
     });
 
     fireEvent.click(await findByText("Complete"));
-    await act(async () => {
-      await waitFor(async () => {
-        expect(spy).toHaveBeenCalled();
-      });
-      // Nasty workaround for the fact that we don't have formdata.
-      if (submitRequest) {
-        const buffer = await submitRequest.arrayBuffer();
-        const decoder = new TextDecoder();
-        const data = new URLSearchParams(decoder.decode(buffer));
-        expect(data.get("first")).toBe("A text");
-        expect(data.get("second")).toBe("Another text");
-      }
+    await waitFor(async () => {
+      expect(spy).toHaveBeenCalled();
     });
+
+    if (submitRequest) {
+      const formData = await submitRequest.formData();
+      expect(formData.get("first")).toBe("A text");
+      expect(formData.get("second")).toBe("Another text");
+    } else {
+      fail("submitRequest was not set");
+    }
   });
+
   it("Route form with form context", async () => {
     const formWithRouteContext = createForm({
       text: textField({
