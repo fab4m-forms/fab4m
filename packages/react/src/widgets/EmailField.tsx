77f15f912b2b431031b7d2cdb6b6a7a8444f0521
Fabian <fabian@sorqvist.nu>
2026-03-08 19:59:49 +0100

    EmailField

--- packages/react/src/widgets/EmailField.tsx
+++ packages/react/src/widgets/EmailField.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
-import { Input, WidgetProps } from "@fab4m/fab4m";
+import { WidgetProps } from "@fab4m/fab4m";
+import { Input } from "../components/Input";
 
 /**
  * An email field widget.
