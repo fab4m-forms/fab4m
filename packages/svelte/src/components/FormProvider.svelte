827b47ebb2fdada6f963fafb4365498bc4896a66
Fabian <fabian@sorqvist.nu>
2026-05-01 06:47:22 +0200

    form provider

---
+++ packages/svelte/src/components/FormProvider.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { Snippet } from "svelte";
+  import {
+    createFormRenderer,
+    FormRenderer,
+    setFormRendererContext,
+  } from "../formrenderer";
+
+  export type FormProviderProps = {
+    renderer: FormRenderer;
+    children: Snippet;
+  };
+  const { renderer, children }: FormProviderProps = $props();
+  $effect(() => {
+    setFormRendererContext(renderer);
+  });
+</script>
+
+{@render children()}
