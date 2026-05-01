cd1aa858ee57df5831034dd08536f4504977e35b
Fabian <fabian@sorqvist.nu>
2026-05-01 06:29:20 +0200

    formcontext

---
+++ packages/svelte/src/components/FormContext.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { setFormRendererContext, type FormRenderer } from "../formrenderer";
+
+  type Props = FormRenderer & {
+    children?: import("svelte").Snippet;
+  };
+
+  let {
+    widgetComponents,
+    multipleWidgetComponents,
+    validatorComponents,
+    children,
+  }: Props = $props();
+
+  $effect(() => {
+    setFormRendererContext({
+      widgetComponents,
+      multipleWidgetComponents,
+      validatorComponents,
+    });
+  });
+</script>
+
+<slot />
